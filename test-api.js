9946a9d517ef79f0e3faae34ac024e5ee19b0ef4
Chandan Usman Gani <chandanusmangani@gmail.com>
2026-07-19 21:24:51 +0530

    CLEAN CLUTTER

--- test-api.js
+++
@@ -1,6 +0,0 @@
-require('./backend/src/lib/load-env');
-const http = require('http');
-const { verifyToken, signToken } = require('./backend/src/utils/jwt');
-
-const token = signToken({ sub: 26, role: 'STUDENT' });
-console.log(token);
